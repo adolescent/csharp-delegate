bef3c297f63e542db7114d2befd7be3da32dc9f3
fujy <engmohsalah@gmail.com>
2015-07-22 16:12:33 +0200

    Initial commit

---
+++ Delegate/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate
+{
+    class Program
+    {
+        //delegate declaration
+        public delegate void printString(string s);
+
+        //one method
+        public static void WriteToScreen(string str)
+        {
+            Console.WriteLine("screen " + str);
+        }
+
+        //two method
+        public static void WriteToFile(string str)
+        {
+            Console.WriteLine("file "+str);
+        }
+
+        static void Main(string[] args)
+        {
+            printString ps1 = new printString(WriteToScreen);
+            printString ps2 = new printString(WriteToFile);
+
+            ps1("Hello World");
+            ps2("Hello World");
+
+            Console.ReadKey();
+        }
+    }
+}
+
